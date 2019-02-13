1489281bd052c9ed29d8a7ea28040fd8a1c0ab80
vagrant <vagrant@homestead>
2019-02-13 01:32:33 +0000

    keyword check triggers added to another database tables

---
+++ database/migrations/2019_02_12_232417_create_trigger_check_keyword_before_ru_pictures_update.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTriggerCheckKeywordBeforeRuPicturesUpdate extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared('
+        CREATE TRIGGER `check_keyword_before_ru_pictures_update` BEFORE UPDATE ON `ru_pictures` FOR EACH ROW
+            BEGIN
+                CALL CheckKeyword(NEW.keyword, @word_check_result);
+
+                IF (!@word_check_result) THEN
+                    SIGNAL SQLSTATE "45000";
+                END IF;
+            END
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared('DROP TRIGGER `check_keyword_before_ru_pictures_update`');
+    }
+}
