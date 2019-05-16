b6d1225b6b18ddb1585dc3f7485d5c44c1d53b9c
vagrant <vagrant@homestead>
2019-05-16 23:55:24 +0000

    admin panel album view optimized, got rid of extra code lines

--- resources/views/adminpages/adminalbum.blade.php
+++ resources/views/adminpages/adminalbum.blade.php
@@ -2,63 +2,53 @@
 
 @section('admincontent')
 
-<article class="admin-panel-main-article">
-    @if ($albumParents == 0)
-        <div class="path-panel">
-            <span class="path-panel-text">@lang('keywords.Path'):</span>
-            @if (App::isLocale('en'))
-                <a href='/admin/albums' class="path-panel-text"> @lang('keywords.Albums')</a>
-            @else
-                <a href='/ru/admin/albums' class="path-panel-text"> @lang('keywords.Albums')</a>
-            @endif
-            <span class="path-panel-text"> /</span>
-        </div>
-    @else
-        <div class="path-panel">
-            <span class="path-panel-text">@lang('keywords.Path'):</span>
-            @if (App::isLocale('en'))
-                <a href='/admin/albums' class="path-panel-text"> @lang('keywords.Albums')</a>
-            @else
-                <a href='/ru/admin/albums' class="path-panel-text"> @lang('keywords.Albums')</a>
-            @endif
-            <span class="path-panel-text"> /</span>
+<article class="admin-panel-main-article">  
+    <div class="path-panel">
+        <span class="path-panel-text">@lang('keywords.Path'):</span>
+        @if (App::isLocale('en'))
+            <a href='/admin/albums' class="path-panel-text"> @lang('keywords.Albums')</a>
+        @else
+            <a href='/ru/admin/albums' class="path-panel-text"> @lang('keywords.Albums')</a>
+        @endif
+        <span class="path-panel-text"> /</span>
+        @if ($albumParents > 0)
             @foreach ($albumParents as $albumParent)
                 @if (App::isLocale('en'))
-                    <a href='/admin/articles/{{ $albumParent->keyWord }}/page/1' class="path-panel-text">{{ $albumParent->albumName }}</a>
+                    <a href='/admin/albums/{{ $albumParent->keyWord }}/page/1' class="path-panel-text">{{ $albumParent->albumName }}</a>
                 @else
-                    <a href='/ru/admin/articles/{{ $albumParent->keyWord }}/page/1' class="path-panel-text">{{ $albumParent->albumName }}</a>
+                    <a href='/ru/admin/albums/{{ $albumParent->keyWord }}/page/1' class="path-panel-text">{{ $albumParent->albumName }}</a>
                 @endif
                 <span class="path-panel-text"> /</span>
             @endforeach
+        @endif
+    </div> 
+    <div>
+        <h2>{{ $headTitle }}</h2>
+    </div>    
+    <div class="admin-panel-albums-add-picture-album-wrapper">
+        <div class="admin-panel-albums-add-picture-album-button">
+            <a href='#' class="admin-panel-albums-add-picture-album-button-link">@lang('keywords.AddPicture')</a>
         </div>
-    @endif
+        <div class="admin-panel-albums-add-picture-album-button">
+            <a href='#' class="admin-panel-albums-add-picture-album-button-link">@lang('keywords.AddAlbum')</a>
+        </div>
+    </div>
     @if ($albums_and_pictures_total_number > 0)
-        <div>
-            <h2>{{ $headTitle }}</h2>
-        </div>
-        <div class="admin-panel-albums-add-picture-album-wrapper">
-            <div class="admin-panel-albums-add-picture-album-button">
-                <a href='#' class="admin-panel-albums-add-picture-album-button-link">@lang('keywords.AddPicture')</a>
-            </div>
-            <div class="admin-panel-albums-add-picture-album-button">
-                <a href='#' class="admin-panel-albums-add-picture-album-button-link">@lang('keywords.AddAlbum')</a>
-            </div>
-        </div>
         <div class="admin-panel-albums-external-pictures-and-albums-wrapper">
             <div class="admin-panel-albums-pictures-and-albums-wrapper">          
-                @foreach ($albums_and_pictures as $album_or_picture)
-                    @if ($album_or_picture->type == 'album')
-                        <div class="admin-panel-albums-picture-and-album-item">
-                            <div class="admin-panel-albums-picture-and-album-title-and-picture-wrapper">
-                                <div>
-                                    <img src="{{ URL::asset('images/icons/regular_folder_small.png') }}">
-                                </div>
-                                <div class="admin-panel-albums-picture-and-album-title">
-                                    <p>{{ $album_or_picture->caption }}</p>
-                                </div>
+                @foreach ($albums_and_pictures as $album_or_picture)   
+                    <div class="admin-panel-albums-picture-and-album-item">
+                        <div class="admin-panel-albums-picture-and-album-title-and-picture-wrapper">
+                            <div>
+                                <img src="{{ $album_or_picture->type == 'album' ? URL::asset('images/icons/regular_folder_small.png') : URL::asset('images/icons/article.png') }}">
                             </div>
-                            <div class="admin-panel-albums-picture-and-album-control-buttons-wrapper">
-                                <div class="admin-panel-albums-picture-and-album-control-buttons">
+                            <div class="admin-panel-albums-picture-and-album-title">
+                                <p>{{ $album_or_picture->caption }}</p>
+                            </div>
+                        </div>
+                        <div class="admin-panel-albums-picture-and-album-control-buttons-wrapper">
+                            <div class="admin-panel-albums-picture-and-album-control-buttons">
+                                @if ($album_or_picture->type == 'album')
                                     <div class="admin-panel-albums-picture-and-album-control-button">
                                         @if (App::isLocale('en'))
                                             <a href='/admin/albums/{{ $album_or_picture->keyWord }}/page/1' class="admin-panel-albums-picture-and-album-control-button-link">@lang('keywords.Open')</a>
@@ -72,22 +62,7 @@
                                     <div class="admin-panel-albums-picture-and-album-control-button">
                                         <a href='#' class="admin-panel-albums-picture-and-album-control-button-link">@lang('keywords.Delete')</a>
                                     </div>
-                                </div>
-                            </div>
-                        </div>
-                    @endif
-                    @if ($album_or_picture->type == 'picture')
-                        <div class="admin-panel-albums-picture-and-album-item">
-                            <div class="admin-panel-albums-picture-and-album-title-and-picture-wrapper">
-                                <div>
-                                    <img src="{{ URL::asset('images/icons/article.png') }}">
-                                </div>
-                                <div class="admin-panel-albums-picture-and-album-title">
-                                    <p>{{ $album_or_picture->caption }}</p>
-                                </div>
-                            </div>
-                            <div class="admin-panel-albums-picture-and-album-control-buttons-wrapper">
-                                <div class="admin-panel-albums-picture-and-album-control-buttons">
+                                @elseif ($album_or_picture->type == 'picture') 
                                     <div class="admin-panel-albums-picture-and-album-control-button">
                                         <a href='#' class="admin-panel-albums-picture-and-album-control-button-link">@lang('keywords.Open')</a>
                                     </div>
@@ -97,10 +72,10 @@
                                     <div class="admin-panel-albums-picture-and-album-control-button">
                                         <a href='#' class="admin-panel-albums-picture-and-album-control-button-link">@lang('keywords.Delete')</a>
                                     </div>
-                                </div>
+                                @endif
                             </div>
                         </div>
-                    @endif
+                    </div>
                 @endforeach     
             </div>
         </div>
@@ -130,17 +105,6 @@
             </div>
         @endif
     @else
-        <div>
-            <h2>{{ $headTitle }}</h2>
-        </div>    
-        <div class="admin-panel-albums-add-picture-album-wrapper">
-            <div class="admin-panel-albums-add-picture-album-button">
-                <a href='#' class="admin-panel-albums-add-picture-album-button-link">@lang('keywords.AddPicture')</a>
-            </div>
-            <div class="admin-panel-albums-add-picture-album-button">
-                <a href='#' class="admin-panel-albums-add-picture-album-button-link">@lang('keywords.AddAlbum')</a>
-            </div>
-        </div>
         <div class="admin-panel-albums-empty-albums-text-wrapper">
             <p>@lang('keywords.EmptySection')</p>
         </div>
