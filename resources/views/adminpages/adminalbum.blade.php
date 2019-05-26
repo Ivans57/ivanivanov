34a3b57cb9fa191a73dd93548db122b775eb5b1d
vagrant <vagrant@homestead>
2019-05-26 17:37:22 +0000

    Finilized paginator for Articles and Folders, also for the same in Admin Panel and for Albums and pictures in Admin Panel

--- resources/views/adminpages/adminalbum.blade.php
+++ resources/views/adminpages/adminalbum.blade.php
@@ -71,29 +71,9 @@
             </div>
         </div>
         @if ($total_number_of_items > $items_amount_per_page)
-            <div class="paginator">
-                    @if ($pagination_info->current_page == 1)
-                        <span class="first-inactive"></span>
-                    @else
-                        <a href="1" class="first-active" title="@lang('pagination.ToFirstPage')"></a>
-                    @endif
-                    @if ($pagination_info->current_page == 1)
-                        <span class="previous-inactive"></span>
-                    @else
-                        <a href="{{ $pagination_info->previous_page }}" class="previous-active" title="@lang('pagination.ToPreviousPage')"></a>
-                    @endif
-                        <span class="pagination-info">{{ $pagination_info->current_page }} @lang('pagination.Of') {{ $pagination_info->number_of_pages }}</span>
-                    @if ($pagination_info->current_page == $pagination_info->number_of_pages)
-                        <span class="next-inactive"></span>
-                    @else
-                        <a href="{{ $pagination_info->next_page }}" class="next-active" title="@lang('pagination.ToNextPage')"></a>
-                    @endif
-                    @if ($pagination_info->current_page == $pagination_info->number_of_pages)
-                        <span class="last-inactive"></span>
-                    @else
-                        <a href="{{ $pagination_info->number_of_pages }}" class="last-active" title="@lang('pagination.ToLastPage')"></a>
-                    @endif
-                </div>
+            <!--As it is impossible to pass an object via slot, we will pass it via attributes-->
+            @component('paginator', ['pagination_info' => $pagination_info])
+            @endcomponent
         @endif
     @else
         <div class="admin-panel-albums-empty-albums-text-wrapper">
