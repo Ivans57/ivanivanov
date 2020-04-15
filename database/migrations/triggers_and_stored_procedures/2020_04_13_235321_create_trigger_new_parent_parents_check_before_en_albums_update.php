8d96297cbffa1e518d22ee5aaf091f1174501896
vagrant <vagrant@homestead>
2020-04-15 01:17:00 +0000

    AP. Album edit. Now can not move an album into itself or into its child albums

---
+++ database/migrations/triggers_and_stored_procedures/2020_04_13_235321_create_trigger_new_parent_parents_check_before_en_albums_update.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTriggerNewParentParentsCheckBeforeEnAlbumsUpdate extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared('
+        CREATE TRIGGER `new_parent_parents_check_before_en_albums_update` BEFORE UPDATE ON `en_albums` FOR EACH ROW
+            BEGIN				
+		IF (NEW.included_in_album_with_id = OLD.id) 
+                    THEN SIGNAL SQLSTATE "45000" SET MESSAGE_TEXT = "The destination album is the same as the source album.";
+		ELSE 
+                    CALL PotetialEnAlbumsParentsParentCheck(OLD.id, NEW.included_in_album_with_id);
+		END IF;				
+            END
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared('DROP TRIGGER `new_parent_parents_check_before_en_albums_update`');
+    }
+}
