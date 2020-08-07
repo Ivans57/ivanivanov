755bc014d6f831556ef6894238a09c7f6b2577ae
vagrant <vagrant@homestead>
2020-08-07 23:37:39 +0000

    AP. When update an article in case its description is longer than 1000 characters, its getting cut down. Two triggers for articles tables have been activated.

---
+++ database/migrations/triggers/2020_08_07_223354_create_trigger_trim_description_before_ru_articles_update.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTriggerTrimDescriptionBeforeRuArticlesUpdate extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared('
+        CREATE TRIGGER `trim_description_before_ru_articles_update` BEFORE UPDATE ON `ru_articles` FOR EACH ROW
+            BEGIN
+                IF ((NEW.article_description != OLD.article_description) OR (OLD.article_description IS NULL) AND
+                    (NEW.article_description IS NOT NULL) AND (LENGTH(NEW.article_description) > 1000)) THEN
+                    SET NEW.article_description = SUBSTRING(NEW.article_description, 1, 1000);
+                END IF;
+            END
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared('DROP TRIGGER `trim_description_before_ru_articles_update`');
+    }
+}
