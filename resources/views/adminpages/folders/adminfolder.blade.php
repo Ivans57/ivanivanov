5fb32278205464541a99c2b8cef1802c7d8fc345
vagrant <vagrant@homestead>
2021-02-07 01:33:44 +0000

    AP. Articles. Search. Styles are ready.

--- resources/views/adminpages/folders/adminfolder.blade.php
+++ resources/views/adminpages/folders/adminfolder.blade.php
@@ -25,24 +25,22 @@
         </div>
     </div>
     @if ($allArticlesAmount > 0 || $allFoldersAmount > 0)
-        <div>
-            {!! Form::label('search_folders', 'Search for Folders', 
+        <div class="admin-panel-articles-search">
+            {!! Form::label('search_folders', Lang::get('keywords.SearchForFolders').':', 
                            ['class' => 'admin-panel-articles-what-to-search-label']) !!}               
             {!! Form::radio('what_to_search', 'folders', 
                            (($what_to_search === 'folders') ? true : false), ['id' => 'search_folders', 
                             'class' => 'admin-panel-articles-what-to-search']); !!}                    
-            {!! Form::label('search_articles', 'Search for Articles', 
+            {!! Form::label('search_articles', Lang::get('keywords.SearchForArticles').':', 
                            ['class' => 'admin-panel-articles-what-to-search-label']) !!}               
             {!! Form::radio('what_to_search', 'articles', 
                            (($what_to_search === 'articles') ? true : false), ['id' => 'search_articles', 
                             'class' => 'admin-panel-articles-what-to-search']); !!}
-        </div>
-        <div class="admin-panel-keywords-search">
             {!! Form::text('search', null, 
-                    ['class' => 'admin-panel-keywords-search-input', 
+                    ['class' => 'admin-panel-articles-search-input', 
                      'placeholder' => Lang::get('keywords.SearchByName').'...', 'name' => 'search', 'id' => 'search']) !!}
             {!! Form::button('<span class="glyphicon glyphicon-search"></span>', 
-                    ['class' => 'admin-panel-keywords-search-button', 'id' => 'search_button', 'title' => Lang::get('keywords.Search'), 
+                    ['class' => 'admin-panel-articles-search-button', 'id' => 'search_button', 'title' => Lang::get('keywords.Search'), 
                      'data-localization' => App::isLocale('en') ? 'en' : 'ru' ]) !!}
         </div>
     @endif
