07a0b2c7e8c0d2f286cf1b7849a80b667943930e
vagrant <vagrant@homestead>
2022-01-19 01:08:23 +0000

    AP. Albums. Function for retrieving all users available to add has been written. Need to test, improve and simplify.

--- app/Http/Repositories/AdminUsersAddEditDeleteForDirectoryRepository.php
+++ app/Http/Repositories/AdminUsersAddEditDeleteForDirectoryRepository.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Repositories;
 
-//use App\User;
+use App\User;
 use App\UserAlbums;
+use App\Album;
+use App\MainkLink;
 //use App\MainLinkUsers;
 //use App\UsersRolesAndStatuses;
 //The line below is required to make query conditions using merged table's fields.
@@ -35,20 +37,83 @@ class AdminUsersAddEditDeleteForDirectoryRepository {
     
     public function get_users_for_add_for_directory($directory_keyword) {
         
-        $users_from_database_and_added_users_ids = $this->get_users_from_database_and_added_users_ids($directory_keyword);
+        $full_and_limited_access_users_for_directory = $this->get_full_and_limited_access_users_ids_for_directory($directory_keyword);
         
-        $users = array();               
-        foreach ($users_from_database_and_added_users_ids->users_from_database as $user_from_database) {
-            if (in_array($user_from_database->id, 
-                         $this->get_user_ids(json_decode(
-                                             $users_from_database_and_added_users_ids->full_and_limited_access_user_ids->full_access_users, 
-                                             true), 
-                                             json_decode(
-                                             $users_from_database_and_added_users_ids->full_and_limited_access_user_ids->limited_access_users, 
-                                             true))) == false) {
-                $users[$user_from_database->id] = $user_from_database->name;
+        $all_users = User::select('id', 'name')->orderBy('name', 'asc')->get();
+        
+        $not_added_user_names = [];
+        
+        foreach ($all_users as $user) {
+            if ((in_array($user->id, $full_and_limited_access_users_for_directory->full_access_users_names) === false) || 
+                (in_array($user->id, $full_and_limited_access_users_for_directory->limited_access_users_names) === false)) {
+                    array_push($not_added_user_names, $user->name);
+                }
+        }
+        return $not_added_user_names;
+    }
+    
+    private function get_full_and_limited_access_users_ids_for_directory($directory_keyword) {
+        
+        $directory_id = Album::select('id')->where('keyword', $directory_keyword)->firstOrFail();
+        
+        $all_user_ids_with_all_added_albums = UserAlbums::
+                    select('user_id', 'en_albums_full_access', 'en_albums_limited_access', 'ru_albums_full_access', 'ru_albums_limited_access')
+                    ->orderBy('user_id', 'asc')->get();
+        
+        $full_and_limited_access_users_names = new FullAndLimitedAccessUsersNames();
+        
+        $full_and_limited_access_users_names->full_access_users_names = [];
+        
+        $full_and_limited_access_users_names->limited_access_users_names = [];
+        
+        foreach ($all_user_ids_with_all_added_albums as $user_id) {
+            //!Also need to add condition for Russian version!
+            $limited_access_users_names_from_user = json_decode($user_id->en_albums_limited_access, true);
+
+            if (in_array($directory_id, $limited_access_users_names_from_user) === true) {
+                array_push($full_and_limited_access_users_names->limited_access_users_names, $user_id->user_id);
+            } else {
+                $current_main_link_id = MainkLink::select('id')->where('keyword', 'Albums')->firstOrFail();
+                $current_main_links_full_access_users = MainLinkUsers::select('full_access_users')->where('links_id', $current_main_link_id)->firstOrFail();
+                $current_main_links_full_access_users_array = json_decode($current_main_links_full_access_users->full_access_users, true);
+                if (in_array($user_id->user_id, $current_main_links_full_access_users_array) === true) {
+                    array_push($full_and_limited_access_users_names->full_access_users_names, $user_id->user_id);
+                } else {
+                    //!Also need to add condition for Russian version!
+                    $full_access_users_names_from_user = json_decode($user_id->en_albums_full_access, true);
+                    if (in_array($directory_id, $full_access_users_names_from_user) === true) {
+                        array_push($full_and_limited_access_users_names->full_access_users_names, $user_id->user_id);
+                    } else {
+                        //Check all parents.
+                        $all_parents_ids_of_directory = $this->get_parents_id_array($directory_id, array());
+                        foreach ($all_parents_ids_of_directory as $parent_id_of_directory) {
+                            if (in_array($parent_id_of_directory, $full_access_users_names_from_user) === true) {
+                                //If user has a full access to at least one of directory's parents, that means the user has full access to this directory.
+                                //No need to check another directories. 
+                                array_push($full_and_limited_access_users_names->full_access_users_names, $user_id->user_id);
+                                break;
+                            }
+                        }
+                    }
+                }
             }
         }
-        return $users;
+        return $full_and_limited_access_users_names;
     }
+    
+    //This function is required to get all parents of some particular directory.
+    private function get_parents_id_array($directory_id, $parents) {
+        
+        $parent_id = Album::select('included_in_album_with_id')->where('id', $directory_id)->firstOrFail();
+        
+        if ($parent_id) {
+            array_push($parents, $parent_id->included_in_album_with_id);
+            $parents_array = $this->get_parents_id_array($parent_id->included_in_album_with_id, $parents);
+            return $parents_array;
+        } else {
+            return $parents;
+        }
+        
+    }
+
 }
